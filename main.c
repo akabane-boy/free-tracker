cb7df9abc19e0c3de474ee7a9d677e1cb8abf409
Beom-Soo Kim <beomsook@proton.me>
2026-07-18 19:39:15 +0900

    add while loop and gitignore

--- main.c
+++ main.c
@@ -9,19 +9,22 @@ int main (int argc, char *argv[])
 	pid_t pid;
 	char *arg[] = {"free", "-m"};
 
-	pid = fork();
-	switch (pid) {
-	case -1:
-		perror("fork");
-		exit(EXIT_FAILURE);
-	case 0:
-		// child
-		printf("fork success\n");
-		execvp(arg[0], arg);
-	default:
-		// parent
-		wait(NULL);
-		printf("child successfully done...\n");
+	while (1) {
+		pid = fork();
+		switch (pid) {
+		case -1:
+			perror("fork");
+			exit(EXIT_FAILURE);
+		case 0:
+			// child
+			printf("fork success\n");
+			execvp(arg[0], arg);
+		default:
+			// parent
+			wait(NULL);
+			printf("-----------------------------\n");
+		}
+		sleep(1);
 	}
 
 
